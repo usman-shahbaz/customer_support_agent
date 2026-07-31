312bafcde67146e4adb11c01d5757c5ccd2dbeba
Usman_Shahbaz <122297454+usman-shahbaz@users.noreply.github.com>
2026-07-31 23:57:43 +0500

    Implement ToolExecutor to execute tools dynamically

--- backend/agent/tool_executor.py
+++ backend/agent/tool_executor.py
@@ -1,1 +1,30 @@
+from tools.company_search import search_company_knowledge
+from tools.order_lookup import get_order_details
 
+
+class ToolExecutor:
+
+    def execute(
+        self,
+        tool_name,
+        tool_input,
+        session,
+    ):
+
+        if tool_name == "search_company_knowledge":
+
+            return search_company_knowledge(
+                tool_input["question"]
+            )
+
+        if tool_name == "get_order_details":
+
+            return get_order_details(
+                tool_input["question"],
+                session,
+            )
+
+        return {
+            "success": False,
+            "message": "Unknown tool."
+        }
