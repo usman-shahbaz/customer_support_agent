84f23529dc32ee7f0ffca7258a4695fa7966c141
Usman_Shahbaz <122297454+usman-shahbaz@users.noreply.github.com>
2026-08-06 23:35:36 +0500

    Add embedding client to interact with Bedrock API

--- backend/embedding_client.py
+++ backend/embedding_client.py
@@ -1,1 +1,29 @@
+import boto3
+import json
 
+from config import AWS_REGION, EMBEDDING_MODEL_ID
+
+
+bedrock = boto3.client(
+    service_name="bedrock-runtime",
+    region_name=AWS_REGION
+)
+
+
+def get_embedding(text: str):
+    body = json.dumps(
+        {
+            "inputText": text
+        }
+    )
+
+    response = bedrock.invoke_model(
+        modelId=EMBEDDING_MODEL_ID,
+        body=body,
+        accept="application/json",
+        contentType="application/json",
+    )
+
+    response_body = json.loads(response["body"].read())
+
+    return response_body["embedding"]
